5dab6263fdec24f9928449c1e931d0464a60660c
Basanta Thapa <justbasanta5@gmail.com>
2018-07-05 18:01:31 +0545

    Initial setup

---
+++ js/main.js
@@ -0,0 +1,4 @@
+let $background = document.getElementById('main-wrapper');
+let $wall = document.getElementById('wall');
+let $ball = document.getElementById('ball');
+let $button = document.getElementById('button');
